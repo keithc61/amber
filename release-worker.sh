a389033b2731b5a56c8c7cb409ce36f99794f786
Herbert Vojčík <herby@mailbox.sk>
2018-08-13 02:43:56 +0200

    Also install root deps when releasing.

--- release-worker.sh
+++ release-worker.sh
@@ -6,6 +6,7 @@
 git reset --hard
 git checkout master
 git clean -d -x -f
+npm install
 (cd lang && npm install)
 (cd sdk && npm install)
 echo -n "Which version are you going to publish [0 to skip]? "
