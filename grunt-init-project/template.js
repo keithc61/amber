fd439d51e4432026b5ab34fe4b2fbee740dcf5ac
Herby Vojčík <herby@mailbox.sk>
2019-02-25 23:01:18 +0100

    sdk: Bump version to 0.12.0.

--- grunt-init-project/template.js
+++ grunt-init-project/template.js
@@ -125,7 +125,7 @@ exports.template = function (grunt, init, done) {
         };
         props.devDependencies = {
             "@ambers/ide-starter-modal": "^0.2.0",
-            "@ambers/sdk": "^0.11.0",
+            "@ambers/sdk": "^0.12.0",
             "@ambers/contrib-legacy": "^0.7.3",
             "@ambers/helios": "^0.10.2",
             "grunt": "^1.0.3",
