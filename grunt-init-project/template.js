5f7b86b470c342515463b7e1f92acf3b8de5d1a6
Herby Vojčík <herby@mailbox.sk>
2019-02-25 23:52:25 +0100

    scaffolding: Bump deps for 0.23.0 & libs.

--- grunt-init-project/template.js
+++ grunt-init-project/template.js
@@ -116,18 +116,18 @@ exports.template = function (grunt, init, done) {
         // A few additional properties.
         props.keywords = ['Amber', 'Smalltalk'];
         props.dependencies = {
-            "@ambers/contrib-jquery": "^0.5.2",
-            "@ambers/contrib-web": "^0.6.5",
-            "@ambers/lang": "^0.22.4",
-            "@ambers/domite": "^0.8.1",
-            "es6-promise": "^4.2.5",
-            "@ambers/silk": "^0.4.4"
+            "@ambers/contrib-jquery": "^0.6.0",
+            "@ambers/contrib-web": "^0.7.0",
+            "@ambers/lang": "^0.23.0",
+            "@ambers/domite": "^0.9.0",
+            "es6-promise": "^4.2.6",
+            "@ambers/silk": "^0.5.1"
         };
         props.devDependencies = {
             "@ambers/ide-starter-modal": "^0.2.0",
             "@ambers/sdk": "^0.12.0",
-            "@ambers/contrib-legacy": "^0.7.3",
-            "@ambers/helios": "^0.10.2",
+            "@ambers/contrib-legacy": "^0.8.0",
+            "@ambers/helios": "^0.11.0",
             "grunt": "^1.0.3",
             "grunt-contrib-clean": "^1.1.0",
             "grunt-contrib-requirejs": "^1.0.0",
