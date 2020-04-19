bde20c397adf768d1a137db50f70c1a5fe0fe6fb
Herby Vojčík <herby@mailbox.sk>
2020-04-20 01:23:43 +0200

    scaffolding: Bump Amber to 0.28.

--- grunt-init-project/template.js
+++ grunt-init-project/template.js
@@ -119,7 +119,7 @@ exports.template = function (grunt, init, done) {
             "@ambers/contrib-jquery": "^0.6.0",
             "@ambers/contrib-web": "^0.8.2",
             "@ambers/domite": "^0.9.0",
-            "@ambers/lang": "^0.27.0",
+            "@ambers/lang": "^0.28.0",
             "@ambers/silk": "^0.6.0",
             "es6-promise": "^4.2.6"
         };
