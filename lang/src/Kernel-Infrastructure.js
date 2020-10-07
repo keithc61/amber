6732602f17ab5921debe2284830c21f86f3a4e3f
Herby Vojčík <herby@mailbox.sk>
2020-10-07 19:34:28 +0200

    Working on 0.29.6

--- lang/src/Kernel-Infrastructure.js
+++ lang/src/Kernel-Infrastructure.js
@@ -4172,14 +4172,14 @@ selector: "version",
 protocol: "accessing",
 //>>excludeStart("ide", pragmas.excludeIdeData);
 args: [],
-source: "version\x0a\x09\x22Answer the version string of Amber\x22\x0a\x09\x0a\x09^ '0.29.5'",
+source: "version\x0a\x09\x22Answer the version string of Amber\x22\x0a\x09\x0a\x09^ '0.29.6-pre'",
 referencedClasses: [],
 //>>excludeEnd("ide");
 pragmas: [],
 messageSends: []
 }, function ($methodClass){ return function (){
 var self=this,$self=this;
-return "0.29.5";
+return "0.29.6-pre";
 
 }; }),
 $globals.SmalltalkImage);
