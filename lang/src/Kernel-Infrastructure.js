962194e5ccbdc7b60a82fb67b61b0123c1bb7667
Herby Vojčík <herby@mailbox.sk>
2020-10-11 20:56:39 +0200

    Release version 0.29.6

--- lang/src/Kernel-Infrastructure.js
+++ lang/src/Kernel-Infrastructure.js
@@ -4200,14 +4200,14 @@ selector: "version",
 protocol: "accessing",
 //>>excludeStart("ide", pragmas.excludeIdeData);
 args: [],
-source: "version\x0a\x09\x22Answer the version string of Amber\x22\x0a\x09\x0a\x09^ '0.29.6-pre'",
+source: "version\x0a\x09\x22Answer the version string of Amber\x22\x0a\x09\x0a\x09^ '0.29.6'",
 referencedClasses: [],
 //>>excludeEnd("ide");
 pragmas: [],
 messageSends: []
 }, function ($methodClass){ return function (){
 var self=this,$self=this;
-return "0.29.6-pre";
+return "0.29.6";
 
 }; }),
 $globals.SmalltalkImage);
